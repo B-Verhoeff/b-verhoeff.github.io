ef38ed509acdee3139ab1a70bbd79716171f8dcb
B-Verhoeff <ultrawebshop@gmail.com>
2025-06-24 00:33:09 +0200

    Updates versie

--- sd23-p02-escaperoom-b1j-dbr/js/main.js
+++
@@ -1,9 +0,0 @@
-//Declaring the play button
-
-const playButton = document.querySelector("#play-button");
-
-//Playbutton function
-
-playButton.addEventListener("click", function () {
-  window.location.assign("room-2-first.html");
-});
